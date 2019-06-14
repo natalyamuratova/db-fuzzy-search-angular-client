14fe758b8b8660197908366611c4d63fe63378be
Natalya Muratova <natalia_muratova@relex.ru>
2019-06-14 19:30:53 +0300

    Highlight active link

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CommonDataService} from '../../services/common-data.service';
+import {NavigationEnd, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
 
-  constructor(commonDataService: CommonDataService) {
+  private routerSubs: Subscription;
+
+  public menuItems = [
+    {
+      text: 'Настройка подключения',
+      link: 'connection',
+      selected: false
+    },
+    {
+      text: 'Поиск дубликатов',
+      link: 'search',
+      selected: false
+    }
+  ];
+
+  constructor(commonDataService: CommonDataService,
+              private router: Router) {
   }
+
+  ngOnInit() {
+    this.routerSubs = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe(
+      (event: NavigationEnd) => {
+        const route = event.urlAfterRedirects;
+        this.menuItems.forEach(menuItem => {
+          menuItem.selected = '/' + menuItem.link === route;
+        });
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubs) {
+      this.routerSubs.unsubscribe();
+    }
+  }
+
 }
